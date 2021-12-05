2e69889788ca01873ddc6291b926817c7f4f9bc7
tjfnunes <tjf.nunes@campus.fct.unl.pt>
2021-12-05 17:17:44 +0000

    Added backend connecting methods on the genericPage.dart class, thus allowing us to load settings directly on the app, as well as pre-seeding the database

--- lib/views/genericPage.dart
+++ lib/views/genericPage.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:zone/Database/dbHandler.dart';
+import 'package:zone/datatypes/settingsObject.dart';
 
 //This is a settings list. It currently has no functionality but it has been built with multiple distinct settings configurations in mind
 
-
-//List<Settings> listSettings = [];
+List<Settings> listSettings = [];
 
 //These are the default values for all the settings when the app is loaded for the first time and there is no data
 
-//Settings currentSettings;
-String currentTheme = "GeoNames";
+Settings currentSettings;
+String currentLanguage = "English";
+String currentTheme = "Dark";
+int dbDirty = 0;
+int autoZoom = 0;
+bool dbEmpty = true;
 
 
 //This common page class is the superclass of almost all pages. It contains methods that are used in more than one page, therefore reducing code redundancy
@@ -21,72 +26,63 @@ abstract class GenericPage extends StatefulWidget {
 
 //The Page State
 class GenericPageState extends State<GenericPage> {
-  //This applies the current settings (such as units, coordinate systems, and auto-zoom) to the page's content
 
 
-  /*Future<void> applySettings() async {
+  //This applies the current settings to the page's content
+
+
+  Future<void> applySettings() async {
     //It fetches the settings from the database
-    List<Map<String, dynamic>> listMap = await DatabaseHelper.instance.queryAllRowsSettings();
+    List<Map<String, dynamic>> listMap = await DBHandler.instance.queryAllRowsSettings();
     setState(() {
       listMap.forEach((map) => putInFirst(Settings.fromMap(map)));
     });
 
     //If there are no settings, it's going to create a new settings configuration and upload it to the database
     if (listSettings.isEmpty) {
-      Settings defaultSettings = new Settings(settingsId: 1, settingsLang: langValue, settingsCoord: coordValue, settingsUnit: unitValue, settingsFreq: freqValue, settingsPower: powerValue, settingsStor: storValue, settingsRecv: recvValue, settingsSnd: sndValue, settingsNote: noteValue, settingsTim: timValue, settingsCur: currentTheme, settingsDirty: dbDirty, settingsZoom: autoZoom);
+      Settings defaultSettings = new Settings(settingsId: 1, settingsLang: currentLanguage, settingsThem: currentTheme, settingsDirty: dbDirty, settingsZoom: autoZoom);
       putInFirst(defaultSettings);
       //This dbDirty allows for an effective theme seeding. It pretty much establishes that the database is no longer uninitialized, and that is has been modified for the first time, therefore it can be seeded
       dbDirty = 1;
       //Updates the settings
       updateSettings();
       //Seed the database
-      DatabaseHelper.instance.seed();
+      DBHandler.instance.seed();
     } else {
       //the database is not empty, so we're going to import the existing settings
       dbEmpty = false;
       Settings currentSettings = listSettings.first;
-      langValue = currentSettings.settingsLang;
-      coordValue = currentSettings.settingsCoord;
-      unitValue = currentSettings.settingsUnit;
-      freqValue = currentSettings.settingsFreq;
-      powerValue = currentSettings.settingsPower;
-      storValue = currentSettings.settingsStor;
-      recvValue = currentSettings.settingsRecv;
-      sndValue = currentSettings.settingsSnd;
-      noteValue = currentSettings.settingsNote;
-      timValue = currentSettings.settingsTim;
-      idValue = currentSettings.settingsId;
-      currentTheme = (currentSettings.settingsCur == null) ? "GeoNames" : currentSettings.settingsCur;
+      currentLanguage = currentSettings.settingsLang;
+      currentTheme = (currentSettings.settingsThem == null) ? "Dark" : currentSettings.settingsThem;
       dbDirty = currentSettings.settingsDirty;
       autoZoom = currentSettings.settingsZoom;
     }
     //the current settings will be the first of the list
     currentSettings = listSettings.first;
   }
-  */
 
   //The method that updates the settings present in the database
-  /*Future<void> updateSettings() async {
+  Future<void> updateSettings() async {
     //The settings object we're going to use in the database
-    Settings updateSettings = new Settings(settingsId: 1, settingsLang: langValue, settingsCoord: coordValue, settingsUnit: unitValue, settingsFreq: freqValue, settingsPower: powerValue, settingsStor: storValue, settingsRecv: recvValue, settingsSnd: sndValue, settingsNote: noteValue, settingsTim: timValue, settingsCur: currentTheme, settingsDirty: dbDirty, settingsZoom: autoZoom);
+    Settings updateSettings = new Settings(settingsId: 1, settingsLang: currentLanguage, settingsThem: currentTheme, settingsDirty: dbDirty, settingsZoom: autoZoom);
 
     //since in this version there is only one settings configuration on the settings list, we call the Put In First method to ensure the list is clean
     putInFirst(updateSettings);
 
     //If there's already a settings object in the database, we're going to update it, otherwise we insert it as a new one
     if (!dbEmpty) {
-      await DatabaseHelper.instance.updateSettings(updateSettings.toMap());
+      await DBHandler.instance.updateSettings(updateSettings.toMap());
     } else {
-      await DatabaseHelper.instance.insertSettings(updateSettings.toMapWithoutId());
+      await DBHandler.instance.insertSettings(updateSettings.toMapWithoutId());
       dbEmpty = false;
     }
-  }*/
+  }
 
   //Clears and Inserts the Settings object as the first one on the List
-  /*void putInFirst(Settings set) {
+  void putInFirst(Settings set) {
     listSettings.clear();
     listSettings.add(set);
-  }*/
+  }
 
   //This is an abstract class. As such, the widget build returns null
   @override
